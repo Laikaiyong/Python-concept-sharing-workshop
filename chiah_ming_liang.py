40a514e486c461804b8b49beae052f05ae2e3cb1
Vandyck <vandycklai@Vandycks-MacBook-Pro.local>
2023-03-24 22:07:52 +0800

    Rename, misnamed'

--- chiah_ming_liang.py
+++ chiah_ming_liang.py
@@ -1,49 +1,67 @@
-### A login function
-## Requirements:
+# ## A login function
+# # Requirements:
 # CLI only
 # Allow only 3 attempt
 # Assume validations needed for both username and password
+from typing import Dict
 
-## Least
-# Conditional
-# Uses of function
-# Object oriented
 
 class Login:
-    def main(self):
-        login = False
-        x = 0
+    user_name: str = "Vandyck Daddy"
+    password: str = "I want the lanyard"
+    login_status: Dict[str, str] = {
+        "status": "",
+        "message": ""
+    }
+    attempt: int = 1
 
-        def _login(username, password) -> bool:
-            if (username == "admin") & (password == "password"):
-                return True
-            else:
-                return False
+    def __check_attempt(self) -> bool:
+        return self.attempt < 4
 
-        while login == False:
-            if x == 3:
-                print("Login limitation reached.")
-                break
+    def __check_match(self, user_name: str, password: str) -> bool:
+        if user_name == self.user_name and password == self.password:
+            return True
+        else:
+            return False
 
-            username = input("Username: ")
+    @staticmethod
+    def __display_message(status: str, message: str) -> None:
+        print(f"""
+Login {status}.
+Message: {message}.      
+        """)
+
+    def __login(self) -> None:
+        while self.__check_attempt():
+            if self.login_status["status"] == "SUCCESS":
+                return
+            print(f"**************************************\n\n"
+                  f"Login attempt: {self.attempt}")
+            user_name = input("User Name: ")
             password = input("Password: ")
-
-            if (username == "") | (password == ""):
-                print("Do not leave blanks. Please try again.")
+            self.attempt += 1
+            if user_name == "" or password == "":
+                self.login_status["status"] = "FAIL"
+                self.login_status["message"] = "No input"
+            elif self.__check_match(user_name, password):
+                self.login_status["status"] = "SUCCESS"
+                self.login_status["message"] = "Yay! Successfully login."
             else:
-                login = _login(username, password)
-                if login == True:
-                    print("Login successful.")
-                    break
-                else:
-                    print("Login failed.")
+                self.login_status["status"] = "FAIL"
+                self.login_status["message"] = "Invalid Credential"
 
-            x += 1
+            self.__display_message(self.login_status["status"], self.login_status["message"])
 
-user = Login()
-user.main()
+    def run(self) -> None:
+        print(
+            """
+Welcome user. Please enter your login credential.
+You'll only have 3 attempts. 
+*Case sensitive*
+            """
+        )
+        self.__login()
 
-        
 
-        
-
+login = Login()
+login.run()
--- yeo_jia_qi.py
+++ yeo_jia_qi.py
@@ -1,67 +1,49 @@
-# ## A login function
-# # Requirements:
+### A login function
+## Requirements:
 # CLI only
 # Allow only 3 attempt
 # Assume validations needed for both username and password
-from typing import Dict
 
+## Least
+# Conditional
+# Uses of function
+# Object oriented
 
 class Login:
-    user_name: str = "Vandyck Daddy"
-    password: str = "I want the lanyard"
-    login_status: Dict[str, str] = {
-        "status": "",
-        "message": ""
-    }
-    attempt: int = 1
+    def main(self):
+        login = False
+        x = 0
 
-    def __check_attempt(self) -> bool:
-        return self.attempt < 4
-
-    def __check_match(self, user_name: str, password: str) -> bool:
-        if user_name == self.user_name and password == self.password:
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def __display_message(status: str, message: str) -> None:
-        print(f"""
-Login {status}.
-Message: {message}.      
-        """)
-
-    def __login(self) -> None:
-        while self.__check_attempt():
-            if self.login_status["status"] == "SUCCESS":
-                return
-            print(f"**************************************\n\n"
-                  f"Login attempt: {self.attempt}")
-            user_name = input("User Name: ")
-            password = input("Password: ")
-            self.attempt += 1
-            if user_name == "" or password == "":
-                self.login_status["status"] = "FAIL"
-                self.login_status["message"] = "No input"
-            elif self.__check_match(user_name, password):
-                self.login_status["status"] = "SUCCESS"
-                self.login_status["message"] = "Yay! Successfully login."
+        def _login(username, password) -> bool:
+            if (username == "admin") & (password == "password"):
+                return True
             else:
-                self.login_status["status"] = "FAIL"
-                self.login_status["message"] = "Invalid Credential"
+                return False
 
-            self.__display_message(self.login_status["status"], self.login_status["message"])
+        while login == False:
+            if x == 3:
+                print("Login limitation reached.")
+                break
 
-    def run(self) -> None:
-        print(
-            """
-Welcome user. Please enter your login credential.
-You'll only have 3 attempts. 
-*Case sensitive*
-            """
-        )
-        self.__login()
+            username = input("Username: ")
+            password = input("Password: ")
 
+            if (username == "") | (password == ""):
+                print("Do not leave blanks. Please try again.")
+            else:
+                login = _login(username, password)
+                if login == True:
+                    print("Login successful.")
+                    break
+                else:
+                    print("Login failed.")
 
-login = Login()
-login.run()
+            x += 1
+
+user = Login()
+user.main()
+
+        
+
+        
+
