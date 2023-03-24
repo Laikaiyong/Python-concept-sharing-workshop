b767aa049baf45f0155d9b27cde7e06b677be48c
Vandyck <vandycklai@Vandycks-MacBook-Pro.local>
2023-03-24 17:23:22 +0800

    submission requirement

---
+++ login.py
@@ -0,0 +1,10 @@
+### A login function
+## Requirements:
+# CLI only
+# Allow only 3 attempt
+# Assume validations needed for both username and password
+
+## Least
+# Conditional
+# Uses of function
+# Object oriented
